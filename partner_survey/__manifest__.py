9aef6f4bb361dbde43541f312a566205297e553b
Goran Kliska <goran.kliska@slobodni-programi.hr>
2017-06-27 15:59:48 +0200

    OCA Update

---
+++ partner_survey/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Tecnativa S.L.
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Partner Survey",
+    'category': 'Marketing',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'survey',
+    ],
+    'data': [
+        'views/res_partner_view.xml'
+    ],
+    'author': 'Tecnativa, '
+              'Camptocamp, '
+              'Odoo Community Association (OCA)',
+    'website': 'http://www.tecnativa.com',
+    'license': 'AGPL-3',
+    'installable': True,
+}
---
+++ partner_survey/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import res_partner
+from . import survey_user_input_line
