65bf74fdeacb6fee78ddf6dca0b0a8e5ef4661b8
Goran Kliska <goran.kliska@slobodni-programi.hr>
2017-07-19 17:05:28 +0200

    OCA Update

---
+++ account_due_list_payment_mode/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Payment due list with payment mode",
+    "version": "10.0.1.0.0",
+    "category": "Generic Modules/Payment",
+    "author": "Obertix Free Solutions, "
+              "Tecnativa, "
+              "Odoo Community Association (OCA),",
+    "website": "https://odoo-community.org/",
+    "depends": [
+        "account_payment_partner",
+        "account_due_list",
+    ],
+    "data": [
+        'views/payment_view.xml',
+    ],
+    'installable': True,
+    "auto_install": False,
+}
---
+++ mass_mailing_custom_unsubscribe/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': "Customizable unsubscription process on mass mailing emails",
+    "summary": "Know unsubscription reasons, track them",
+    'category': 'Marketing',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'website_mass_mailing',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/mail_unsubscription_reason.xml',
+        'templates/general_reason_form.xml',
+        'templates/mass_mailing_contact_reason.xml',
+        'views/assets.xml',
+        'views/mail_unsubscription_reason_view.xml',
+        'views/mail_mass_mailing_list_view.xml',
+        'views/mail_unsubscription_view.xml',
+    ],
+    "demo": [
+        'demo/assets.xml',
+    ],
+    'images': [
+        'images/form.png',
+    ],
+    'author': 'Tecnativa,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://www.tecnativa.com',
+    'license': 'AGPL-3',
+    'installable': True,
+}
