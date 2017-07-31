d904658adb56db157c450c5263ebf36de3f0bc0f
Goran Kliska <goran.kliska@slobodni-programi.hr>
2017-07-31 15:16:21 +0200

    OCA Update

---
+++ account_renumber/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2009 Pexego Sistemas Informáticos. All Rights Reserved
+# Copyright 2013 Pedro Manuel Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2013 Joaquin Gutierrez <http://www.gutierrezweb.es>
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2017 David Vidal <david.vidal@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': "Account Renumber Wizard",
+    'version': "10.0.1.0.0",
+    'author': "Pexego,Tecnativa,Odoo Community Association (OCA)",
+    'website': "http://www.pexego.es",
+    'category': "Accounting & Finance",
+    "license": "AGPL-3",
+    "depends": [
+        'account',
+    ],
+    "data": [
+        'wizard/wizard_renumber_view.xml',
+    ],
+    'installable': True,
+}
---
+++ l10n_es_location_nuts/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Tecnativa - Antonio Espinosa
+# Copyright 2015 Tecnativa - Jairo Llopis
+# Copyright 2017 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'NUTS Regions for Spain',
+    'summary': 'NUTS specific options for Spain',
+    'version': '10.0.1.0.0',
+    'category': 'Localisation/Europe',
+    'website': 'https://www.tecnativa.com',
+    'author': 'Tecnativa, '
+              'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'base_location_nuts',
+    ],
+    'post_init_hook': 'post_init_hook',
+}
