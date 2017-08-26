5adcf6701bcf7385a866065210850b8ebf3ecd2b
Goran Kliska <goran.kliska@slobodni-programi.hr>
2017-08-26 15:47:14 +0200

    OCA Update

---
+++ account_move_template/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2017 See manifest
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': "Account Move Template",
+    'version': '10.0.2.0.0',
+    'category': 'Generic Modules/Accounting',
+    'summary': "Templates for recurring Journal Entries",
+    'author': "Agile Business Group,Odoo Community Association (OCA), Aurium "
+              "Technologies,Vauxoo",
+    'website': 'http://www.agilebg.com , http://www.auriumtechnologies.com',
+    'license': 'AGPL-3',
+    'depends': ['account_accountant', 'analytic'],
+    'data': [
+        'security/ir.model.access.csv',
+        'view/move_template.xml',
+        'wizard/select_template.xml',
+    ],
+    'test': [
+    ],
+    'installable': True,
+}
