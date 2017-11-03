9ab3a3cd711e222570e6f1ea0cbdfe1a32cb786f
Goran Kliska <goran.kliska@slobodni-programi.hr>
2017-11-03 13:29:08 +0100

    OCA Update

---
+++ business_requirement_deliverable_project_task_categ/wizard/generate_projects.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2017 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import api, models
+
+
+class BrGenerateProjects(models.TransientModel):
+    _inherit = 'br.generate.projects'
+
+    @api.multi
+    def _prepare_project_task(self, line, project_id):
+        vals = super(BrGenerateProjects, self) \
+            ._prepare_project_task(line, project_id)
+        if line.categ_id:
+            vals.update({
+                'categ_id': line.categ_id.id
+            })
+        return vals
---
+++ invoice_comment_template/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 Guewen Baconnier (Camptocamp SA)
+# Copyright 2013-2014 Nicolas Bessi (Camptocamp SA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{'name': 'Invoice Comments',
+ 'summary': 'Comments templates on invoice documents',
+ 'version': '10.0.1.0.0',
+ 'depends': ['account',
+             'base_comment_template',
+             ],
+ 'author': "Camptocamp,Odoo Community Association (OCA)",
+ "license": "AGPL-3",
+ 'data': ['views/account_invoice_view.xml',
+          'views/base_comment_template_view.xml',
+          'security/ir.model.access.csv',
+          'views/report_invoice.xml',
+          ],
+ 'category': 'Sale',
+ 'installable': True,
+ }
