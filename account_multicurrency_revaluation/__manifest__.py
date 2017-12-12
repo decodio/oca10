2a085f08690d5bc1a5716e92beb0d37c2c2e2a09
Goran Kliska <goran.kliska@slobodni-programi.hr>
2017-12-12 19:00:06 +0100

    OCA Update

--- account_multicurrency_revaluation/__manifest__.py
+++ account_multicurrency_revaluation/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {"name": "Multicurrency revaluation",
- "version": "10.0.1.0.0",
+ "version": "10.0.2.0.1",
  "category": "Finance",
  "summary": "Manage revaluation for multicurrency environment",
  "author": "Camptocamp,Odoo Community Association (OCA)",
@@ -19,10 +19,14 @@
      "views/res_config_view.xml",
      "security/security.xml",
      "views/account_view.xml",
-     "wizard/wizard_currency_revaluation_view.xml"
+     "wizard/print_currency_unrealized_report_view.xml",
+     "wizard/wizard_currency_revaluation_view.xml",
+     "report/report.xml",
+     "report/unrealized_currency_gain_loss.xml",
  ],
  "tests": [
      "tests/test_currency_revaluation.py",
+     "tests/test_currency_revaluation_report.py"
  ],
  'installable': True,
  }
